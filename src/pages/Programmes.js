9436d397fa3c3397f176465996ace5bc06c95a0e
Peter Hill <petehilljnr@gmail.com>
2022-04-29 12:54:11 +1200

    progress update 2

--- src/pages/Programmes.js
+++ src/pages/Programmes.js
@@ -1,32 +1,104 @@
 import { useState } from "react";
-import {
-  Container, Button
-} from "react-bootstrap";
+import { Container, Button, Row, Col } from "react-bootstrap";
 
 import { PageTitle } from "../components/PageTitle";
+import { Sponsors } from "../components/Sponsors";
 import { createPrintOut } from "../templates/standard";
+import styled from "styled-components";
+
 // import styled from "styled-components";
 
 import layout1img from "../assets/layout1.png";
 import layout2img from "../assets/layout2.png";
 import { Templates } from "../components/Templates";
+import Select from "react-select";
+
+const SubTitle = styled.div`
+  text-align: center;
+  color: white;
+  text-shadow: -2px 0 black, 0 2px black, 2px 0 black, 0 -2px black;
+  font-size: 1.3rem;
+`;
 
 export const Programmes = () => {
   const [insideLeftValue, setInsideLeftValue] = useState("layout1");
-  const [insideRightValue, setInsideRightValue] = useState("layout1");
-  //const handleClick = () => standardTemplate({ player_names: player_names });
+  const [insideRightValue, setInsideRightValue] = useState("layout2");
+  const [outsideRightValue, setOutsideRightValue] = useState("layout1");
+  const [selectedSponsors, setSelectedSponsors] = useState([]);
+  
   const layouts = {
     layout1: { name: "Standard with photos", img: layout1img },
     layout2: { name: "List with no photos", img: layout2img },
   };
 
-  const onClick = () => { createPrintOut(null)}
+  const handleSponsorSelect = (e) => {
+    setSelectedSponsors(e);
+  };
+
+  const onClick = () => {
+    //addSponsors(0, (11.7/2)-0.19685, 8.3,selectedSponsors)
+    createPrintOut({
+        fixture_id: 0,
+        il_layout: insideLeftValue,
+        ir_layout: insideRightValue,
+        sponsors: selectedSponsors
+    });
+    
+  };
+
+console.log(selectedSponsors)
   return (
     <Container>
       <PageTitle text="Printed Programmes" />
-    <Button onClick={onClick}>Click</Button>
-      <Templates prefix="IL" key={"IL"} title="Inside Left Layout" currentValue={insideLeftValue} setCurrentValue={setInsideLeftValue} layouts={layouts} />
-      <Templates prefix="IR" key={"IR"} title="Inside Right Layout" currentValue={insideRightValue} setCurrentValue={setInsideRightValue} layouts={layouts} />
+      <Button onClick={onClick}>Click</Button>
+      <Row>
+        <Col>
+          <Templates
+            prefix="IL"
+            key={"IL"}
+            title="Inside Left Layout"
+            currentValue={insideLeftValue}
+            setCurrentValue={setInsideLeftValue}
+            layouts={layouts}
+          />
+        </Col>
+        <Col>
+          <Templates
+            prefix="IR"
+            key={"IR"}
+            title="Inside Right Layout"
+            currentValue={insideRightValue}
+            setCurrentValue={setInsideRightValue}
+            layouts={layouts}
+          />
+        </Col>
+      </Row>
+
+      <Row>
+        <Col>
+          <SubTitle> Select Game Sponsors </SubTitle>
+          <Select
+            isMulti
+            name="game_sponsors"
+            options={Sponsors}
+            className="basic-multi-select"
+            classNamePrefix="select"
+            maxMenuHeight={200}
+            value={selectedSponsors}
+            onChange={handleSponsorSelect}
+          />
+        </Col>
+        <Col>
+          <Templates
+            prefix="OR"
+            key={"OR"}
+            title="Front Page"
+            currentValue={outsideRightValue}
+            setCurrentValue={setOutsideRightValue}
+            layouts={layouts}
+          />
+        </Col>
+      </Row>
     </Container>
   );
 };
--- src/templates/standard.js
+++ src/templates/standard.js
@@ -49,7 +49,7 @@ const standardNoPhotosLogo = function (
 
   slide.addText("RESERVES", {
     x: left_edge,
-    y: colStartY + (8.5 * rowHeight),
+    y: colStartY + 8.5 * rowHeight,
     w: right_edge - left_edge,
     h: 0.5,
     valign: "top",
@@ -61,7 +61,7 @@ const standardNoPhotosLogo = function (
   });
 
   for (let i = 1; i <= 8; i++) {
-    const startY = colStartY + (i-1) * rowHeight;
+    const startY = colStartY + (i - 1) * rowHeight;
     const startX = gutterX + left_edge;
 
     slide.addText(i, {
@@ -127,7 +127,7 @@ const standardNoPhotosLogo = function (
   }
 
   for (let i = 16; i <= 19; i++) {
-    const startY = colStartY + (9 * rowHeight) + (i-15) * rowHeight
+    const startY = colStartY + 9 * rowHeight + (i - 15) * rowHeight;
     const startX = gutterX + left_edge;
 
     slide.addText(i, {
@@ -160,7 +160,7 @@ const standardNoPhotosLogo = function (
   }
 
   for (let i = 20; i <= 22; i++) {
-    const startY = colStartY + (9 * rowHeight) + (i-19) * rowHeight
+    const startY = colStartY + 9 * rowHeight + (i - 19) * rowHeight;
     const startX = gutterX + middleX + left_edge;
 
     slide.addText(i, {
@@ -201,10 +201,19 @@ const standardWithPhotos = function (
   right_edge,
   logo
 ) {
-  const colStartY = 0.75;
-  const rowHeight = 1.85;
+  const colStartY = 1.5;
+  const rowHeight = 1.68;
   const colWidth = (right_edge - left_edge) / 5;
   const imageHeight = 1.3;
+  
+  const sample_images = [
+    process.env.PUBLIC_URL + "/images/no_image_grey.png",
+    process.env.PUBLIC_URL + "/images/players/600545.png",
+    process.env.PUBLIC_URL + "/images/players/3358780.png",
+    process.env.PUBLIC_URL + "/images/players/3432698.png",
+    process.env.PUBLIC_URL + "/images/players/1747522.png",
+    process.env.PUBLIC_URL + "/images/players/miller.png"
+  ];
 
   // Add Team Title
   slide.addText(team_name, {
@@ -251,20 +260,21 @@ const standardWithPhotos = function (
       const startY = colStartY + i * rowHeight;
       const startX = colWidth * j; // + mm2i(2);
 
-      const imagePath = process.env.PUBLIC_URL + "/images/no_image.png";
+      //const imagePath = process.env.PUBLIC_URL + "/images/no_image.png";
+      const imagePath = sample_images[Math.floor(Math.random() * sample_images.length)];
 
       slide.addImage({
         path: imagePath,
-        x: startX + left_edge,
+        x: (startX + left_edge),
         y: startY,
-        sizing: { type: "contain", w: colWidth, h: imageHeight },
+        sizing: { type: "contain", w: colWidth },
       });
 
       const pos = i * 5 + j + 1;
 
       slide.addText(pos, {
         x: startX + left_edge,
-        y: startY + imageHeight + mm2i(1),
+        y: startY + mm2i(27),
         h: mm2i(6),
         w: mm2i(5),
         fontFace: "Impact",
@@ -283,7 +293,7 @@ const standardWithPhotos = function (
         ).toUpperCase(),
         {
           x: startX + mm2i(6) + left_edge,
-          y: startY + imageHeight + mm2i(1),
+          y: startY + mm2i(27),
           w: colWidth - mm2i(5),
           h: 0.8,
           fontFace: "Bahnschrift SemiLight SemiConde",
@@ -301,15 +311,22 @@ const standardWithPhotos = function (
     const startX = colWidth * j * (5 / 7); //+2mm
     const imageHeightSmall = imageHeight * (5 / 7);
 
-    const imagePath = process.env.PUBLIC_URL + "/images/no_image.png";
+    const imagePath = sample_images[Math.floor(Math.random() * 5)];
 
+    slide.addImage({
+      path: imagePath,
+      x: (startX + left_edge),
+      y: startY,
+      sizing: { type: "contain", w: colWidth * (5 / 7) * 0.9 },
+    });
+    /*
     slide.addImage({
       path: imagePath,
       x: startX + left_edge,
       y: startY,
       sizing: { type: "contain", w: colWidth * (5 / 7), h: imageHeightSmall },
     });
-
+    */
     const pos = 15 + j + 1;
 
     slide.addText(pos, {
@@ -347,6 +364,74 @@ const standardWithPhotos = function (
   return slide;
 };
 
+const addSponsors = function (
+  slide,
+  left_edge,
+  right_edge,
+  slide_height,
+  sponsors
+) {
+
+  const gutters =  mm2i(5)
+  const startX = left_edge + gutters;
+  const usableWidth = (right_edge - left_edge) - (2 * gutters);
+  const usableHeight = slide_height - (2 * gutters);
+  const numSponsors = sponsors.length;
+   
+  sponsors.forEach((s) => {
+    const ratio = usableWidth / s.w
+    const new_height = (s.h * ratio)
+    const new_width = (s.w * ratio)
+
+    s.new_height = new_height;
+    s.new_width = new_width;
+    s.hw_ratio = s.h / s.w;    
+  });
+
+  const minHeight = Math.min(...sponsors.map(i => i.new_height))
+
+  sponsors.forEach((s) => {
+      const ratio = (minHeight * 1.5) / s.new_height;
+
+      s.new_height = ratio < 1 ? s.new_height * ratio : s.new_height;
+      s.new_width = ratio < 1 ? s.new_width * ratio : s.new_width;
+
+  })
+  
+  const totalHeight = sponsors.map(i => i.new_height).reduce((prev, next) => prev + next) + (sponsors.length || 0 * gutters);
+
+  if(totalHeight > slide_height) {
+    const ratio = slide_height / totalHeight;
+    sponsors.forEach((s) => {
+      s.new_height = s.new_height * ratio;
+      s.new_width = s.new_width * ratio;
+    })
+  }
+  let cumStart = 0;
+
+  for(let i = 0; i < sponsors.length; i++) {
+    sponsors[i].startY = cumStart;
+    cumStart = cumStart + sponsors[i].new_height + gutters;
+  };
+
+  console.log(sponsors)
+
+  for (let i = 0; i < sponsors.length; i++) {
+    const imagePath =
+      process.env.PUBLIC_URL + "/images/sponsors/" + sponsors[i].value;
+    const startY = sponsors[i].startY;
+    slide.addImage({
+      path: imagePath,
+      x: ((right_edge - left_edge) / 2) - (sponsors[i].new_width / 2),
+      y: startY,
+      h: sponsors[i].new_height,
+      w: sponsors[i].new_width
+    });
+  }
+
+  return slide;
+};
+
 export const createPrintOut = function (options) {
   let pptx = new pptxgen();
 
@@ -380,29 +465,61 @@ export const createPrintOut = function (options) {
   let left_edge = 0;
   let right_edge = left_edge + slideWidth.i / 2 - printMargin.i;
 
-  slide_one = standardWithPhotos(
-    slide_one,
-    "KAIKORAI",
-    player_names,
-    left_edge,
-    right_edge
-  );
+  slide_one =
+    options.il_layout === "layout1"
+      ? standardWithPhotos(
+          slide_one,
+          "KAIKORAI",
+          player_names,
+          left_edge,
+          right_edge
+        )
+      : standardNoPhotosLogo(
+          slide_one,
+          "KAIKORAI",
+          player_names,
+          "",
+          left_edge,
+          right_edge
+        );
 
   // first slide - right side
-
+  console.log(options);
   left_edge = slideWidth.i / 2 + printMargin.i;
   right_edge = slideWidth.i;
 
-  // eslint-disable-next-line
-  slide_one = standardNoPhotosLogo(
-    slide_one,
-    "KAIKORAI",
-    player_names,
-    "",
+  slide_one =
+    options.ir_layout === "layout1"
+      ? standardWithPhotos(
+          slide_one,
+          "KAIKORAI",
+          player_names,
+          left_edge,
+          right_edge
+        )
+      : standardNoPhotosLogo(
+          slide_one,
+          "KAIKORAI",
+          player_names,
+          "",
+          left_edge,
+          right_edge
+        );
+
+  // second slide - left side - back page when folded
+
+  let slide_two = pptx.addSlide({ masterName: "MASTER_SLIDE" });
+
+  left_edge = 0;
+  right_edge = left_edge + slideWidth.i / 2 - printMargin.i;
+
+  slide_two = addSponsors(
+    slide_two,
     left_edge,
-    right_edge
+    right_edge,
+    slideHeight,
+    options.sponsors
   );
-  
 
   // output teamsheet to download
   pptx.writeFile({ fileName: "test.pptx " });
